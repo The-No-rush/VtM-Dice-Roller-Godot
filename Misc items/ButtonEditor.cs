ebf362f0b802784526c6ebf78354b56d1e0ec611
= <ethan.norush@protonmail.com>
2023-04-29 21:48:24 -0400

    Started get button ID's  on press to set custom rolls to buttons

--- Misc items/ButtonEditor.cs
+++ Misc items/ButtonEditor.cs
@@ -1,11 +1,23 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 public partial class ButtonEditor : Node2D
 {
+	private List<Button> selectableButtons = new();
+
+	private Callable actionFunctionCallable;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		actionFunctionCallable = new Callable(this, nameof(SetActionToButton));
+		for (int i = 0; i < 4; i++)
+		{
+			Button button = GetNode<Button>("CustomButton" + (i + 1));
+			button.Pressed += () => SetActionToButton(button.Text.Substring(button.Text.Length -1)); // Can't use i
+			//with lambda so for temporary time using the texts substring
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,5 +28,10 @@ public partial class ButtonEditor : Node2D
 	private void SwitchToDiceRoller()
 	{
 		GetTree().ChangeSceneToPacked((PackedScene)ResourceLoader.Load("res://Main.tscn"));
+	}
+
+	public void SetActionToButton(Variant buttonIndex)
+	{
+		GD.Print(buttonIndex);
 	}
 }
